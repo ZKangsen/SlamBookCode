ce99fdaaaf8a6c0d40bad6870123710f39a61a7b
kenny <454091685@qq.com>
2019-10-19 20:56:18 +0800

    homework1 finished

--- deepblue_homework/homework1/main.cpp
+++ deepblue_homework/homework1/main.cpp
@@ -5,39 +5,55 @@
 #include <Eigen/Geometry>
 #include <sophus/so3.h>
 
+Eigen::Matrix3d UpdateRotationMatrix(const Eigen::Matrix3d& R,
+                               const Eigen::Vector3d& w,
+                               bool use_sophus = false);
+
+Eigen::Quaterniond UpdateQuaterniond(const Eigen::Quaterniond& q,
+                                     const Eigen::Vector3d& w);
+
 int main(int argc, char** argv) {
   // init log
   google::InitGoogleLogging(argv[0]);
   google::LogToStderr();
 
-  // update rotation
+  // Test the difference of rotation updates between matrix and quaterniond
   Eigen::Matrix3d R = Eigen::AngleAxisd(M_PI / 3, Eigen::Vector3d(1, 0, 0)).toRotationMatrix();
-  LOG(INFO) << "origin R = \n" << R;
+  Eigen::Quaterniond Q(R);
   Eigen::Vector3d w(0.01, 0.02, 0.03);
-  Sophus::SO3 SO3_R(R);
-  Sophus::SO3 SO3_updated = SO3_R * Sophus::SO3::exp(w);
-  Eigen::Matrix3d R_updated = SO3_updated.matrix();
-  LOG(INFO) << "after R update, R = \n" << R_updated;
-
-  // do not use sophus
-  double theta = w.norm();
-  Eigen::Vector3d a = w / theta;
-  Eigen::Matrix3d aaT = a * a.transpose();
-  Eigen::Matrix3d a_hat;
-  a_hat << 0, -a.z(), a.y(),
-           a.z(), 0, -a.x(),
-           -a.y(), a.x(), 0;
-  Eigen::Matrix3d delta_R =
-      cos(theta) * Eigen::Matrix3d::Identity() + (1 - cos(theta)) * aaT + sin(theta) * a_hat;
-  Eigen::Matrix3d R_updated2 = R * delta_R;
-  LOG(INFO) << "R_updated = \n" << R_updated;
-
-  // use q
-  Eigen::Quaterniond q = Eigen::Quaterniond(R);
-  Eigen::Quaterniond w_q(1, w.x() / 2, w.y() / 2, w.z() / 2);
-  Eigen::Quaterniond q_updated = q * w_q;
-  LOG(INFO) << "after q update, R = \n" << q_updated.toRotationMatrix();
-
-
+  Eigen::Matrix3d R_updated = UpdateRotationMatrix(R, w);
+  Eigen::Quaterniond Q_updated = UpdateQuaterniond(Q, w);
+  LOG(INFO) << "origin_R = \n" << R;
+  LOG(INFO) << "After updating with rotation matrix, R_updated = \n" << R_updated;
+  LOG(INFO) << "After updating with quaternion, R_updated = \n" << Q_updated.toRotationMatrix();
   return 0;
+}
+
+Eigen::Matrix3d UpdateRotationMatrix(const Eigen::Matrix3d& R,
+                                     const Eigen::Vector3d& w,
+                                     bool use_sophus) {
+  Eigen::Matrix3d R_updated(R);
+  if(use_sophus) {
+    Sophus::SO3 SO3_R(R);
+    Sophus::SO3 SO3_updated = SO3_R * Sophus::SO3::exp(w);
+    R_updated = SO3_updated.matrix();
+  } else {
+    double theta = w.norm();
+    Eigen::Vector3d n = w / theta;
+    Eigen::Matrix3d nnT = n * n.transpose();
+    Eigen::Matrix3d n_hat;
+    n_hat << 0, -n.z(), n.y(),
+             n.z(), 0, -n.x(),
+             -n.y(), n.x(), 0;
+    Eigen::Matrix3d update_R =
+        cos(theta) * Eigen::Matrix3d::Identity() + (1 - cos(theta)) * nnT + sin(theta) * n_hat;
+    R_updated = R * update_R;
+  }
+  return R_updated;
+}
+
+Eigen::Quaterniond UpdateQuaterniond(const Eigen::Quaterniond& q, const Eigen::Vector3d& w) {
+  Eigen::Quaterniond q_w(1, w.x() / 2, w.y() / 2, w.z() / 2);
+  q_w.normalize();
+  return q * q_w;
 }

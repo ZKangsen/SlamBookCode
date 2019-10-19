e823ef70a5dcf590f3f120a2a1a828ad6597e9db
ZKangsen <454091685@qq.com>
2019-10-19 15:07:38 +0800

    add deepblue homework

--- deepblue_homework/homework1/main.cpp
+++ deepblue_homework/homework1/main.cpp
@@ -19,6 +19,19 @@ int main(int argc, char** argv) {
   Eigen::Matrix3d R_updated = SO3_updated.matrix();
   LOG(INFO) << "after R update, R = \n" << R_updated;
 
+  // do not use sophus
+  double theta = w.norm();
+  Eigen::Vector3d a = w / theta;
+  Eigen::Matrix3d aaT = a * a.transpose();
+  Eigen::Matrix3d a_hat;
+  a_hat << 0, -a.z(), a.y(),
+           a.z(), 0, -a.x(),
+           -a.y(), a.x(), 0;
+  Eigen::Matrix3d delta_R =
+      cos(theta) * Eigen::Matrix3d::Identity() + (1 - cos(theta)) * aaT + sin(theta) * a_hat;
+  Eigen::Matrix3d R_updated2 = R * delta_R;
+  LOG(INFO) << "R_updated = \n" << R_updated;
+
   // use q
   Eigen::Quaterniond q = Eigen::Quaterniond(R);
   Eigen::Quaterniond w_q(1, w.x() / 2, w.y() / 2, w.z() / 2);
@@ -28,4 +41,3 @@ int main(int argc, char** argv) {
 
   return 0;
 }
-
